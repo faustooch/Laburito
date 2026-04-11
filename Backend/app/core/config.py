1374fa0d1539d158b8b42840890331252abf616d
faustooch <faustooch@gmail.com>
2026-04-11 04:33:38 -0300

    Update config

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -1,12 +1,15 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import field_validator
 
+
 class Settings(BaseSettings):
     PROJECT_NAME: str
     API_V1_STR: str
+
+    # Credenciales de Google Auth
     GOOGLE_CLIENT_ID: str
     GOOGLE_CLIENT_SECRET: str
-    
+
     @field_validator("GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET", mode="before")
     @classmethod
     def strip_quotes_and_spaces(cls, v: str) -> str:
@@ -14,19 +17,15 @@ class Settings(BaseSettings):
             return v.strip(' "\'')
         return v
 
-    POSTGRES_SERVER: str
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
-    POSTGRES_PORT: str
+    # Credenciales de Base de Datos (¡Ahora es una sola línea!)
+    DATABASE_URL: str
 
-    @property
-    def database_url(self) -> str:
-        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
-
+    # Seguridad
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
 
 settings = Settings()
